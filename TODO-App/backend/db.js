efa8b90717411bd76a63f9e46d5edca284a6001f
Nishant840 <nishant7896545@gmail.com>
2025-12-12 01:01:39 +0530

    developed backend for todo app

--- TODO-App/backend/db.js
+++ TODO-App/backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://nishant7896545_db_user:Nishant1234kumar@cluster0.ohlk6k2.mongodb.net/todosApp")
-const todoSchema = mongoose.schema({
+const todoSchema = mongoose.Schema({
     title: String,
     desciption: String,
     completed: Boolean
