176d9e1525d9f2d3faf17a86e8e9a9b875c2a1b2
Nishant840 <nishant7896545@gmail.com>
2025-12-12 00:29:42 +0530

    making a todo app in react

--- TODO-App/backend/index.js
+++ TODO-App/backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", function(req,res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if(!parsedPayload.success()){
+        res.send(411).json({
+            msg: "you send the wrong inputs"
+        })
+        return;
+    }
+    // if parsed than put this in mongodb
 
 })
 
@@ -13,7 +23,16 @@ app.get("/todos", function(req,res){
 })
 
 app.put("/completed",function(req,res){
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success()){
+        res.send(411).json({
+            msg: "you send the wrong inputs"
+        })
+        return;
+    }
+    // update todo in mongodb
+    
 })
 
 const port = process.env.PORT || 3000;
