96c2a82ac83b9f856f05772001fd2aecb9c3a4bf
Nishant840 <nishant7896545@gmail.com>
2025-12-12 00:42:53 +0530

    Added db logic

---
+++ TODO-App/backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://nishant7896545_db_user:Nishant1234kumar@cluster0.ohlk6k2.mongodb.net/todosApp")
+const todoSchema = mongoose.schema({
+    title: String,
+    desciption: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos',todoSchema);
+module.exports = {
+    todo
+}
