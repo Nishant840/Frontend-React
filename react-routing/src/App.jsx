acf53169f3b2de87ff3af1805d1a5c3c1241e031
Nishant840 <nishant7896545@gmail.com>
2025-12-16 17:55:13 +0530

    hard reloading, routing using window.location.href=

---
+++ react-routing/src/App.jsx
@@ -0,0 +1,28 @@
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Dashboard } from "./components/Dashboard"
+import { Landing } from "./components/Landing"
+
+
+function App() {
+  return (
+    <div>
+        <div>
+            <button onClick={()=>{
+                window.location.href = "/";
+            }} >Landing Page</button>
+            <button onClick={()=>{
+                window.location.href = "/dashboard"
+            }} >Dashboard Page</button>
+        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard/>} />
+                <Route path="/" element={<Landing/>} />
+            </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
