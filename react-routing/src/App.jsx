2762df96683bdf80166ea734ba6ebc478932b322
Nishant840 <nishant7896545@gmail.com>
2025-12-16 18:35:14 +0530

    learning useNavigate hook in react

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,21 +1,38 @@
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
 import { Dashboard } from "./components/Dashboard"
 import { Landing } from "./components/Landing"
 
 
+// function App() {
+//   return (
+//     <div>
+//         <div>
+//             <button onClick={()=>{
+//                 // window.location.href = "/";  this will do hard reloading means page will get from backend again
+//             }} >Landing Page</button>
+//             <button onClick={()=>{
+//                 window.location.href = "/dashboard"
+//             }} >Dashboard Page</button>
+//         </div>
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/dashboard" element={<Dashboard/>} />
+//                 <Route path="/" element={<Landing/>} />
+//             </Routes>
+//         </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// useNavigate() hook always use it in inside BrowserRouter tag
 function App() {
   return (
     <div>
-        <div>
-            <button onClick={()=>{
-                window.location.href = "/";
-            }} >Landing Page</button>
-            <button onClick={()=>{
-                window.location.href = "/dashboard"
-            }} >Dashboard Page</button>
-        </div>
         <BrowserRouter>
+            <TopBar/>
             <Routes>
                 <Route path="/dashboard" element={<Dashboard/>} />
                 <Route path="/" element={<Landing/>} />
@@ -25,4 +42,15 @@ function App() {
   )
 }
 
+function TopBar(){
+    const navigate = useNavigate();
+    return <div>
+        <button onClick={()=>{
+            navigate("/")
+        }} >Landing Page</button>
+        <button onClick={()=>{
+            navigate("/dashboard")
+        }} >Dashboard Page</button>
+    </div>
+}
 export default App
