727a63d7a35785e40b5afaee2920ad307cd33ddc
Nishant840 <nishant7896545@gmail.com>
2025-12-17 00:55:56 +0530

    learning context API in react

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -2,7 +2,7 @@
 import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
 // import { Dashboard } from "./components/Dashboard"
 // import { Landing } from "./components/Landing"
-import React from "react";
+import React, { useContext } from "react";
 import { lazy, Suspense} from "react";
 
 
@@ -97,36 +97,86 @@ import { lazy, Suspense} from "react";
 // understanding prop drilling: when a component needed prop but many of it's parent doesn/t need it
 // we still pass it this is prop drilling it makes codes unmanagable, and ugly/less_ readable
 
+// import { useState } from "react"
+
+// function App() {
+//   const [count, setCount] = useState(0);
+  
+//   return (
+//     <div>
+//       <Count count={count} setCount={setCount}/>
+//     </div>
+//   )
+// }
+
+// function Count({count, setCount}) {
+//     // setcount not needed here but button component needed so we have to take it as props this is prop drilling
+//   return <div>
+//     {count}
+//     <Buttons count={count} setCount={setCount}  />
+//   </div>
+// }
+
+// function Buttons({count,setCount}) {
+//   return <div>
+//     <button onClick={() => {
+//         setCount(count => count+1)
+//     }}>Increase</button>
+
+//     <button onClick={() => {
+//         setCount(count => count-1)
+//     }}>Decrease</button>
+//   </div>
+// }
+
+// export default App
+
+
+// context API
+
+//  it let us teleport prop from one component to another without explicitally passing through each component.
+// it helps in gets rid of prop drilling 
+// it also let us keep all state logic outside of main react component
+
 import { useState } from "react"
+import { CounterContext } from "./context";
 
 function App() {
   const [count, setCount] = useState(0);
   
-  return (
-    <div>
-      <Count count={count} setCount={setCount}/>
+    //   wrap anyone that wants to use the teleported value inside the provider
+    return (
+        <div>
+            <CounterContext.Provider value={count} >
+            <Count setCount={setCount}/>
+            </CounterContext.Provider>
+        </div>
+    )
+}
+
+function Count({setCount}) {
+    return <div>
+        <CountRender />
+        <Buttons setCount={setCount}  />
     </div>
-  )
 }
-
-function Count({count, setCount}) {
-    // setcount not needed here but button component needed so we have to take it as props this is prop drilling
-  return <div>
-    {count}
-    <Buttons count={count} setCount={setCount}  />
-  </div>
+function CountRender(){
+    const count = useContext(CounterContext)
+    return <div>
+        {count}
+    </div>
 }
+function Buttons({setCount}){
+    return <div>
+        <button onClick={() => {
+            setCount(count => count+1)
+        }}>Increase</button>
 
-function Buttons({count,setCount}) {
-  return <div>
-    <button onClick={() => {
-        setCount(count => count+1)
-    }}>Increase</button>
-
-    <button onClick={() => {
-        setCount(count => count-1)
-    }}>Decrease</button>
-  </div>
+        <button onClick={() => {
+            setCount(count => count-1)
+        }}>Decrease</button>
+    </div>
 }
 
 export default App
+
