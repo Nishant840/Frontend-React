09b794bc2d66f238e47b463b2c542312c90657e3
Nishant840 <nishant7896545@gmail.com>
2025-12-16 21:25:41 +0530

    learning lazy loading in react

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,7 +1,9 @@
 
 import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
-import { Dashboard } from "./components/Dashboard"
-import { Landing } from "./components/Landing"
+// import { Dashboard } from "./components/Dashboard"
+// import { Landing } from "./components/Landing"
+import React from "react";
+import { lazy, Suspense} from "react";
 
 
 // function App() {
@@ -28,20 +30,55 @@ import { Landing } from "./components/Landing"
 // export default App
 
 // useNavigate() hook always use it in inside BrowserRouter tag
-function App() {
-  return (
-    <div>
+// function App() {
+//   return (
+//     <div>
+//         <BrowserRouter>
+//             <TopBar/>
+//             <Routes>
+//                 <Route path="/dashboard" element={<Dashboard/>} />
+//                 <Route path="/" element={<Landing/>} />
+//             </Routes>
+//         </BrowserRouter>
+//     </div>
+//   )
+// }
+
+// function TopBar(){
+//     const navigate = useNavigate();
+//     return <div>
+//         <button onClick={()=>{
+//             navigate("/")
+//         }} >Landing Page</button>
+//         <button onClick={()=>{
+//             navigate("/dashboard")
+//         }} >Dashboard Page</button>
+//     </div>
+// }
+// export default App
+
+
+
+// lazy loading
+
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
+
+function App(){
+    return <div>
         <BrowserRouter>
-            <TopBar/>
-            <Routes>
-                <Route path="/dashboard" element={<Dashboard/>} />
-                <Route path="/" element={<Landing/>} />
-            </Routes>
+            <TopBar />
+            <Suspense fallback={<div>Loading...</div>}>
+            {/* React.lazy requires Suspense because lazy components are asynchronous, and React needs a 
+                boundary to pause rendering while waiting for them. */}
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard/>} />
+                    <Route path="/" element={<Landing/>} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     </div>
-  )
 }
-
 function TopBar(){
     const navigate = useNavigate();
     return <div>
@@ -53,4 +90,5 @@ function TopBar(){
         }} >Dashboard Page</button>
     </div>
 }
+
 export default App
