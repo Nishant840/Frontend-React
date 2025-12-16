f68829bb689d1b7789f95c69547a314178583bdb
Nishant840 <nishant7896545@gmail.com>
2025-12-17 00:13:42 +0530

    learning prop derilling in react

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -61,34 +61,72 @@ import { lazy, Suspense} from "react";
 
 // lazy loading
 
-const Dashboard = lazy(() => import("./components/Dashboard"));
-const Landing = lazy(() => import("./components/Landing"));
+// const Dashboard = lazy(() => import("./components/Dashboard"));
+// const Landing = lazy(() => import("./components/Landing"));
 
-function App(){
-    return <div>
-        <BrowserRouter>
-            <TopBar />
-            <Suspense fallback={<div>Loading...</div>}>
-            {/* React.lazy requires Suspense because lazy components are asynchronous, and React needs a 
-                boundary to pause rendering while waiting for them. */}
-                <Routes>
-                    <Route path="/dashboard" element={<Dashboard/>} />
-                    <Route path="/" element={<Landing/>} />
-                </Routes>
-            </Suspense>
-        </BrowserRouter>
+// function App(){
+//     return <div>
+//         <BrowserRouter>
+//             <TopBar />
+//             <Suspense fallback={<div>Loading...</div>}>
+//             {/* React.lazy requires Suspense because lazy components are asynchronous, and React needs a 
+//                 boundary to pause rendering while waiting for them. */}
+//                 <Routes>
+//                     <Route path="/dashboard" element={<Dashboard/>} />
+//                     <Route path="/" element={<Landing/>} />
+//                 </Routes>
+//             </Suspense>
+//         </BrowserRouter>
+//     </div>
+// }
+// function TopBar(){
+//     const navigate = useNavigate();
+//     return <div>
+//         <button onClick={()=>{
+//             navigate("/")
+//         }} >Landing Page</button>
+//         <button onClick={()=>{
+//             navigate("/dashboard")
+//         }} >Dashboard Page</button>
+//     </div>
+// }
+
+// export default App
+
+
+// understanding prop drilling: when a component needed prop but many of it's parent doesn/t need it
+// we still pass it this is prop drilling it makes codes unmanagable, and ugly/less_ readable
+
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      <Count count={count} setCount={setCount}/>
     </div>
+  )
 }
-function TopBar(){
-    const navigate = useNavigate();
-    return <div>
-        <button onClick={()=>{
-            navigate("/")
-        }} >Landing Page</button>
-        <button onClick={()=>{
-            navigate("/dashboard")
-        }} >Dashboard Page</button>
-    </div>
+
+function Count({count, setCount}) {
+    // setcount not needed here but button component needed so we have to take it as props this is prop drilling
+  return <div>
+    {count}
+    <Buttons count={count} setCount={setCount}  />
+  </div>
+}
+
+function Buttons({count,setCount}) {
+  return <div>
+    <button onClick={() => {
+        setCount(count => count+1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+        setCount(count => count-1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
