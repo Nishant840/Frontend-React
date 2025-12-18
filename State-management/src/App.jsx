68a83e5a6b897ecf95073df2f4bd256c7a8cdc3c
Nishant840 <nishant7896545@gmail.com>
2025-12-18 23:41:03 +0530

    learning atoms and selectors in recoil

--- State-management/src/App.jsx
+++ State-management/src/App.jsx
@@ -1,59 +1,94 @@
 
-// context API let us get rid of prop drilling but it doesn't solve re-rendering problem
+// // context API let us get rid of prop drilling but it doesn't solve re-rendering problem
+
+// import { useRecoilState, useRecoilValue, useSetRecoilState, RecoilRoot } from "recoil"
+// import { countAtom, evenSelector } from "../store/atoms/count"
+// import { useMemo } from "react";
+
+
+// function App() {
+//     return (
+//         <div>
+//             <RecoilRoot>
+//                 <Count />
+//             </RecoilRoot>
+//         </div>
+//     )
+// }
+
+// function Count() {
+//     return <div>
+//         <CountRender />
+//         <Buttons />
+//     </div>
+// }
+// function CountRender(){
+//     const count = useRecoilValue(countAtom);
+//     return <div>
+//         {count}
+//         <EvenCountRenderer />
+//     </div>
+// }
+// function EvenCountRenderer(){
+//     // const count = useRecoilValue(countAtom);
+//     // const isEven = useMemo(()=>{
+//     //     return (count%2 == 0);
+//     // },[count])
+
+//     const isEven = useRecoilValue(evenSelector);
+//     return <div>
+//         {(isEven)?"It is Even":null}
+//     </div>
+// }
+// function Buttons(){
+// //   const [count,setCount] = useRecoilState(countAtom);
+//     const setCount = useSetRecoilState(countAtom);
+//     return <div>
+//         <button onClick={() => {
+//             // setCount(count+1);
+//             setCount(count=>count+1);
+//         }}>Increase</button>
+
+//         <button onClick={() => {
+//             // setCount(count-1);
+//             setCount(count=>count-1);
+//         }}>Decrease</button>
+//     </div>
+// }
+
+// export default App
+
+
+
+
+// making top bar of linkedIn
 
 import { useRecoilState, useRecoilValue, useSetRecoilState, RecoilRoot } from "recoil"
-import { countAtom, evenSelector } from "../store/atoms/count"
-import { useMemo } from "react";
+import { jobAtom, networkAtom, notificationAtom, totalCountSelector } from "../store/atoms/count"
 
 
-function App() {
-    return (
-        <div>
-            <RecoilRoot>
-                <Count />
-            </RecoilRoot>
-        </div>
-    )
+function App(){
+    return <RecoilRoot>
+        <MainApp />
+    </RecoilRoot>
 }
 
-function Count() {
-    return <div>
-        <CountRender />
-        <Buttons />
+function MainApp(){
+    const networkCount = useRecoilValue(networkAtom);
+    const jobsCount = useRecoilValue(jobAtom);
+    const messageCount = useRecoilValue(jobAtom);
+    const notificationCount = useRecoilValue(notificationAtom);
+
+    const totalCount = useRecoilValue(totalCountSelector);
+
+    return <div style={{margin: "20px"}} >
+        <button>Home</button>
+
+        <button>My Network ({networkCount > 99 ? "99+" : networkCount })</button>
+        <button>Jobs ({jobsCount}) </button>
+        <button>Messagings ({messageCount}) </button>
+        <button>Notifications ({notificationCount}) </button>
+        <button>me ({totalCount}) </button>
     </div>
 }
-function CountRender(){
-    const count = useRecoilValue(countAtom);
-    return <div>
-        {count}
-        <EvenCountRenderer />
-    </div>
-}
-function EvenCountRenderer(){
-    // const count = useRecoilValue(countAtom);
-    // const isEven = useMemo(()=>{
-    //     return (count%2 == 0);
-    // },[count])
-
-    const isEven = useRecoilValue(evenSelector);
-    return <div>
-        {(isEven)?"It is Even":null}
-    </div>
-}
-function Buttons(){
-//   const [count,setCount] = useRecoilState(countAtom);
-    const setCount = useSetRecoilState(countAtom);
-    return <div>
-        <button onClick={() => {
-            // setCount(count+1);
-            setCount(count=>count+1);
-        }}>Increase</button>
-
-        <button onClick={() => {
-            // setCount(count-1);
-            setCount(count=>count-1);
-        }}>Decrease</button>
-    </div>
-}
-
 export default App
--- State-management/store/atoms/count.jsx
+++ State-management/store/atoms/count.jsx
@@ -1,18 +1,50 @@
 
+// import {atom, selector} from "recoil"
+
+
+// export const countAtom = atom({
+//     key: "countAtom",
+//     default: 0
+// });
+// // key is used to indetify atom uniquely
+
+// // selectors
+// export const evenSelector = selector({
+//     key: "evenSelector",
+//     get: (props)=>{
+//         const count = props.get(countAtom);
+//         return count%2==0;
+//     }
+// })
+
+// atoms of topbar of LinkedIn
+
 import {atom, selector} from "recoil"
+export const networkAtom= atom({
+    key: "networkAtom",
+    default:102
+})
 
-
-export const countAtom = atom({
-    key: "countAtom",
+export const jobAtom = atom({
+    key: "jobAtom",
     default: 0
-});
-// key is used to indetify atom uniquely
+})
+export const notificationAtom = atom({
+    key: "notificationAtom",
+    default: 2
+})
+export const messageAtom = atom({
+    key: "messageAtom",
+    default: 0
+})
 
-// selectors
-export const evenSelector = selector({
-    key: "evenSelector",
-    get: (props)=>{
-        const count = props.get(countAtom);
-        return count%2==0;
+export const totalCountSelector = selector({
+    key: "totalCount",
+    get: ({get})=>{
+        const networkCount = get(networkAtom);
+        const jobsCount = get(jobAtom);
+        const messageCount = get(jobAtom);
+        const notificationCount = get(notificationAtom);
+        return networkCount+jobsCount+messageCount+notificationCount;
     }
 })
